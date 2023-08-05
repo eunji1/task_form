9c81b087d27c5a937e88f4a842175104d39ceee5
eunji1 <ssak1088@gmail.com>
2023-08-05 22:52:23 +0900

    css+

--- src/components/DatepickerCustom/DatepickerRenderCustomHeader.tsx
+++ src/components/DatepickerCustom/DatepickerRenderCustomHeader.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { getYear } from 'date-fns';
-// import { getMonth, getYear } from 'date-fns';
+import { getMonth, getYear } from 'date-fns';
 import range from 'lodash/range';
 
 interface DatepickerHeaderType {
   date: Date;
-  //   changeYear: () => string;
+  changeYear: (year: number) => void;
   changeMonth: (month: number) => void;
   decreaseMonth: () => void;
   increaseMonth: () => void;
@@ -14,54 +13,52 @@ interface DatepickerHeaderType {
 }
 const DatepickerRenderCustomHeader: React.FC<DatepickerHeaderType> = ({
   date,
-  //   changeYear,
-  //   changeMonth,
+  changeYear,
+  changeMonth,
   decreaseMonth,
   increaseMonth,
   prevMonthButtonDisabled,
   nextMonthButtonDisabled
 }) => {
-  const years = range(2023, getYear(new Date()) + 1, 1);
+  const years = range(getYear(new Date()), 2014, -1);
 
   return (
-    <div className='bg-white'>
+    <div className='bg-white flex gap-1 items-center justify-center'>
       <button
         type='button'
         onClick={decreaseMonth}
         disabled={prevMonthButtonDisabled}
+        className='w-4 h-4'
       >
         <img src='img/calender_back_default.png'></img>
       </button>
       <select
         value={getYear(date)}
-        // onChange={({ target: { value } }) => changeYear(value)}
-        className=''
+        onChange={({ target: { value } }) => changeYear(Number(value))}
+        className='text-sm px-3 py-1 border-[1px] border-[#DEDEDE] rounded'
       >
         {years.map((option) => (
           <option key={option} value={option}>
-            {option}
+            {option}년
           </option>
         ))}
       </select>
-      <span className=''>년</span>
       <select
-        // value={Number(MONTH_CONST.LIST[getMonth(date)])}
-        // onChange={({ target: { value } }) =>
-        //   changeMonth(MONTH_CONST.LIST.indexOf(value))
-        // }
-        className=''
+        value={getMonth(date)}
+        onChange={({ target: { value } }) => changeMonth(Number(value))}
+        className='text-sm px-3 py-1 border-[1px] border-[#DEDEDE] rounded'
       >
-        {/* {MONTH_CONST.LIST.map((option) => (
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((option) => (
           <option key={option} value={option}>
-            {option}
+            {option}월
           </option>
-        ))} */}
+        ))}
       </select>
-      <span className=''>월</span>
       <button
         type='button'
         onClick={increaseMonth}
         disabled={nextMonthButtonDisabled}
+        className='w-4 h-4'
       >
         <img src='img/calender_next_default.png'></img>
       </button>
